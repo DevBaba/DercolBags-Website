52b48721a2333ac626d41d6163e8a8824c21f721
Ransford Yeboah <132473175+DevBaba@users.noreply.github.com>
2023-11-12 20:59:39 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { Link, animateScroll as scroll } from "react-scroll";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
